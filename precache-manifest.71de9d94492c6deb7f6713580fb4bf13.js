d24c31fc3f0763ab9ff35904cb631fc22f44ae66
WillCKyle <williamofnewe@gmail.com>
2020-11-09 09:18:09 -0600

    Updates

---
+++ precache-manifest.71de9d94492c6deb7f6713580fb4bf13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b904bd7e10d84ee9858a446d5bc07b6b",
+    "url": "/odomo-frontend/index.html"
+  },
+  {
+    "revision": "58ecfb9fa8a7bbf6938a",
+    "url": "/odomo-frontend/static/css/main.6c80f5de.chunk.css"
+  },
+  {
+    "revision": "67dd854ecb72f161179c",
+    "url": "/odomo-frontend/static/js/2.a826ef64.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/odomo-frontend/static/js/2.a826ef64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58ecfb9fa8a7bbf6938a",
+    "url": "/odomo-frontend/static/js/main.2be19840.chunk.js"
+  },
+  {
+    "revision": "27836ae2925a731a9291",
+    "url": "/odomo-frontend/static/js/runtime-main.991adc24.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/odomo-frontend/precache-manifest.71de9d94492c6deb7f6713580fb4bf13.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/odomo-frontend/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
