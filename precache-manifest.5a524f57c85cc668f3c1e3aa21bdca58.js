7e150ebac7936a5571603a2f04a25ca305a67d9c
WillCKyle <williamofnewe@gmail.com>
2020-11-12 18:06:55 -0600

    Updates

---
+++ precache-manifest.5a524f57c85cc668f3c1e3aa21bdca58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3efebd0c2adcecefe32d3bb4cfb16aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c4ec1e9a60e6b239175",
+    "url": "/static/css/main.6c80f5de.chunk.css"
+  },
+  {
+    "revision": "67dd854ecb72f161179c",
+    "url": "/static/js/2.a826ef64.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a826ef64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c4ec1e9a60e6b239175",
+    "url": "/static/js/main.692fdcef.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/odomo-frontend/precache-manifest.6d044c1cba3292de0a7103a7b80f931a.js"
+  "/precache-manifest.5a524f57c85cc668f3c1e3aa21bdca58.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/odomo-frontend/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
